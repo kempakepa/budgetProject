da0e3cd5c9b180ab6ed3f4e7d822bdcd32c46685
Katarzyna Kepa <katarzyna.kepa90@gmail.com>
2022-08-02 13:39:36 +0200

    removed function trash parameter from calls in tests

--- cypress/integration/api/cashFlowManager.test.js
+++ cypress/integration/api/cashFlowManager.test.js
@@ -4,89 +4,71 @@ const {
 
 describe('cashFlowManager test', () => {
     it('should return 200 status code if add cost body parameters valid', () => {
-        sendRequestToAddCostItem(
-            {
-                title: 'jabadaba',
-                comment: 'mpk',
-                date: '2022-02-01',
-                amount: 3454,
-                category: 'Food',
-            },
-            true
-        ).then((response) => {
+        sendRequestToAddCostItem({
+            title: 'jabadaba',
+            comment: 'mpk',
+            date: '2022-02-01',
+            amount: 3454,
+            category: 'Food',
+        }).then((response) => {
             expect(response.status).to.equal(200);
         });
     });
 
     it('should return 400 status code if no amount in /addCostItem request body', () => {
-        sendRequestToAddCostItem(
-            {
-                title: 'jabadaba',
-                comment: 'mpk',
-                date: '2022-02-01',
-                //amount: 3454,
-                category: 'Food',
-            },
-            false
-        ).then((response) => {
+        sendRequestToAddCostItem({
+            title: 'jabadaba',
+            comment: 'mpk',
+            date: '2022-02-01',
+            //amount: 3454,
+            category: 'Food',
+        }).then((response) => {
             expect(response.status).to.equal(400);
         });
     });
     it('should return 400 status code if no title in /addCostItem request body', () => {
-        sendRequestToAddCostItem(
-            {
-                //title: 'jabadaba',
-                comment: 'mpk',
-                date: '2022-02-01',
-                amount: 3454,
-                category: 'Food',
-            },
-            false
-        ).then((response) => {
+        sendRequestToAddCostItem({
+            //title: 'jabadaba',
+            comment: 'mpk',
+            date: '2022-02-01',
+            amount: 3454,
+            category: 'Food',
+        }).then((response) => {
             expect(response.status).to.equal(400);
         });
     });
     it('should return 400 status code if no comment in /addCostItem request body', () => {
-        sendRequestToAddCostItem(
-            {
-                title: 'jabadaba',
-                //comment: 'mpk',
-                date: '2022-02-01',
-                amount: 3454,
-                category: 'Food',
-            },
-            false
-        ).then((response) => {
+        sendRequestToAddCostItem({
+            title: 'jabadaba',
+            //comment: 'mpk',
+            date: '2022-02-01',
+            amount: 3454,
+            category: 'Food',
+        }).then((response) => {
             expect(response.status).to.equal(400);
         });
     });
 
     it('should return 400 status code if no date in /addCostItem request body', () => {
-        sendRequestToAddCostItem(
-            {
-                title: 'jabadaba',
-                comment: 'mpk',
-                //date: '2022-02-01',
-                amount: 3454,
-                category: 'Food',
-            },
-            false
-        ).then((response) => {
+        sendRequestToAddCostItem({
+            title: 'jabadaba',
+            comment: 'mpk',
+            //date: '2022-02-01',
+            amount: 3454,
+            category: 'Food',
+        }).then((response) => {
             expect(response.status).to.equal(400);
         });
     });
 
     it('should return 400 status code if no category in /addCostItem request body', () => {
-        sendRequestToAddCostItem(
-            {
-                title: 'jabadaba',
-                comment: 'mpk',
-                date: '2022-02-01',
-                amount: 3454,
-                //category: 'Food',
-            },
-            false
-        ).then((response) => {
+        sendRequestToAddCostItem({
+            title: 'jabadaba',
+            comment: 'mpk',
+            date: '2022-02-01',
+            amount: 3454,
+            //category: 'Food',
+        }).then((response) => {
             expect(response.status).to.equal(400);
         });
     });
