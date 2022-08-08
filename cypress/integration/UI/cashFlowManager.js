9f87b3b16a74fec849cff65de48bf889a19007df
Katarzyna Kepa <katarzyna.kepa90@gmail.com>
2022-08-08 21:22:20 +0200

    pipeline update, cashflowManager tests upgrade

--- cypress/integration/UI/cashFlowManager.js
+++ cypress/integration/UI/cashFlowManager.js
@@ -12,12 +12,113 @@ describe('cashFlowManager tests', () => {
             amount: 100,
             category: 'Food',
         });
-        /* CashFlowManagerObject.selectCost();
-        CashFlowManagerObject.insertTitle('title');
-        CashFlowManagerObject.insertComment('comment');
-        CashFlowManagerObject.insertDate('2022-07-30');
-        CashFlowManagerObject.insertAmount(100);
-        CashFlowManagerObject.insertCategory('category');
-        CashFlowManagerObject.submitCashFlowManagerForm(); */
+        cy.get('[data-cy="submition_message"]')
+            .invoke('text')
+            .should('equal', 'Cost added successfully');
     });
+
+    /* it('addCost tests - invalid title input', () => {
+        HomePageObject.visitHomePage();
+        HomePageObject.goToCashFlowManagerModule();
+        CashFlowManagerObject.addCost({
+            //title: 'title',
+            comment: 'comment',
+            date: '2022-02-02',
+            amount: 100,
+            category: 'Food',
+        });
+        cy.get('[data-cy="submition_message"]')
+            .invoke('text')
+            .should('equal', 'Invalid input');
+    });
+
+    it('addCost tests - invalid comment input', () => {
+        HomePageObject.visitHomePage();
+        HomePageObject.goToCashFlowManagerModule();
+        CashFlowManagerObject.addCost({
+            title: 'title',
+            //comment: 'comment',
+            date: '2022-02-02',
+            amount: 100,
+            category: 'Food',
+        });
+        cy.get('[data-cy="submition_message"]')
+            .invoke('text')
+            .should('equal', 'Invalid input');
+    });
+
+    it('addCost tests - invalid date input', () => {
+        HomePageObject.visitHomePage();
+        HomePageObject.goToCashFlowManagerModule();
+        CashFlowManagerObject.addCost({
+            title: 'title',
+            comment: 'comment',
+            //date: '2022-02-02',
+            amount: 100,
+            category: 'Food',
+        });
+        cy.get('[data-cy="submition_message"]')
+            .invoke('text')
+            .should('equal', 'Invalid input');
+    });
+
+    it('addCost tests - invalid amount input', () => {
+        HomePageObject.visitHomePage();
+        HomePageObject.goToCashFlowManagerModule();
+        CashFlowManagerObject.addCost({
+            title: 'title',
+            comment: 'comment',
+            date: '2022-02-02',
+            //amount: 100,
+            category: 'Food',
+        });
+        cy.get('[data-cy="submition_message"]')
+            .invoke('text')
+            .should('equal', 'Invalid input');
+    });
+
+    it('addCost tests - invalid category input', () => {
+        HomePageObject.visitHomePage();
+        HomePageObject.goToCashFlowManagerModule();
+        CashFlowManagerObject.addCost({
+            title: 'title',
+            comment: 'comment',
+            date: '2022-02-02',
+            amount: 100,
+            //category: 'Food',
+        });
+        cy.get('[data-cy="submition_message"]')
+            .invoke('text')
+            .should('equal', 'Invalid input');
+    });
+
+    it('addCost tests - invalid empty title input', () => {
+        HomePageObject.visitHomePage();
+        HomePageObject.goToCashFlowManagerModule();
+        CashFlowManagerObject.addCost({
+            title: '',
+            comment: 'comment',
+            date: '2022-02-02',
+            amount: 100,
+            category: 'Food',
+        });
+        cy.get('[data-cy="submition_message"]')
+            .invoke('text')
+            .should('equal', 'Invalid input');
+    });
+
+    it('addCost tests - invalid empty comment input', () => {
+        HomePageObject.visitHomePage();
+        HomePageObject.goToCashFlowManagerModule();
+        CashFlowManagerObject.addCost({
+            title: 'title',
+            comment: '',
+            date: '2022-02-02',
+            amount: 100,
+            category: 'Food',
+        });
+        cy.get('[data-cy="submition_message"]')
+            .invoke('text')
+            .should('equal', 'Invalid input');
+    }); */
 });
--- cypress/integration/api/cashFlowManager.test.js
+++ cypress/integration/api/cashFlowManager.test.js
@@ -1,5 +1,6 @@
 const {
     sendRequestToAddCostItem,
+    sendRequestToAddIncomeItem,
 } = require('../../services/cashFlowManagerService');
 
 describe('cashFlowManager test', () => {
@@ -12,6 +13,7 @@ describe('cashFlowManager test', () => {
             category: 'Food',
         }).then((response) => {
             expect(response.status).to.equal(200);
+            expect(response.body).to.equal('Cost added successfully');
         });
     });
 
@@ -24,6 +26,7 @@ describe('cashFlowManager test', () => {
             category: 'Food',
         }).then((response) => {
             expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
         });
     });
     it('should return 400 status code if no title in /addCostItem request body', () => {
@@ -35,6 +38,7 @@ describe('cashFlowManager test', () => {
             category: 'Food',
         }).then((response) => {
             expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
         });
     });
     it('should return 400 status code if no comment in /addCostItem request body', () => {
@@ -46,6 +50,7 @@ describe('cashFlowManager test', () => {
             category: 'Food',
         }).then((response) => {
             expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
         });
     });
 
@@ -58,6 +63,7 @@ describe('cashFlowManager test', () => {
             category: 'Food',
         }).then((response) => {
             expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
         });
     });
 
@@ -70,6 +76,83 @@ describe('cashFlowManager test', () => {
             //category: 'Food',
         }).then((response) => {
             expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
+        });
+    });
+
+    it('should return 200 status code if add income body parameters valid', () => {
+        sendRequestToAddIncomeItem({
+            title: 'jabadaba',
+            comment: 'mpk',
+            date: '2022-02-01',
+            amount: 3454,
+            category: 'Food',
+        }).then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body).to.equal('Income added successfully');
+        });
+    });
+
+    it('should return 400 status code if no amount in /addIncomeItem request body', () => {
+        sendRequestToAddIncomeItem({
+            title: 'jabadaba',
+            comment: 'mpk',
+            date: '2022-02-01',
+            //amount: 3454,
+            category: 'Food',
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
+        });
+    });
+    it('should return 400 status code if no title in /addIncomeItem request body', () => {
+        sendRequestToAddIncomeItem({
+            //title: 'jabadaba',
+            comment: 'mpk',
+            date: '2022-02-01',
+            amount: 3454,
+            category: 'Food',
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
+        });
+    });
+    it('should return 400 status code if no comment in /addIncomeItem request body', () => {
+        sendRequestToAddIncomeItem({
+            title: 'jabadaba',
+            //comment: 'mpk',
+            date: '2022-02-01',
+            amount: 3454,
+            category: 'Food',
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
+        });
+    });
+
+    it('should return 400 status code if no date in /addIncomeItem request body', () => {
+        sendRequestToAddIncomeItem({
+            title: 'jabadaba',
+            comment: 'mpk',
+            //date: '2022-02-01',
+            amount: 3454,
+            category: 'Food',
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
+        });
+    });
+
+    it('should return 400 status code if no category in /addIncomeItem request body', () => {
+        sendRequestToAddIncomeItem({
+            title: 'jabadaba',
+            comment: 'mpk',
+            date: '2022-02-01',
+            amount: 3454,
+            //category: 'Food',
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body).to.equal('Invalid input');
         });
     });
 });
