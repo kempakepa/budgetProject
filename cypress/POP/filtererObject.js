b87215cfed2ce356e8c544dab7d7abe3255f1090
pbielenda <p.bielenda@evionica.com>
2022-09-12 09:58:25 +0200

    feat(#2): add filterer page object

---
+++ cypress/POP/filtererObject.js
@@ -0,0 +1,23 @@
+class FiltererObject {
+    static fillCategory(category) {
+        cy.get('#category').type(category);
+    }
+
+    static clickSend() {
+        cy.get('[type="button"]').click();
+    }
+
+    static incomeShouldBe(expectedValue) {
+        cy.get('[data-cy="incomeSummary"]').should('have.text', expectedValue);
+    }
+
+    static costsShouldBe(expectedValue) {
+        cy.get('[data-cy="costSummary"]').should('have.text', expectedValue);
+    }
+
+    static balanceShouldBe(expectedValue) {
+        cy.get('[data-cy="balanceSummary"]').should('have.text', expectedValue);
+    }
+}
+
+module.exports = { FiltererObject };
