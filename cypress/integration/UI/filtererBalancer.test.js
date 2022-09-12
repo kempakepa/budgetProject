f148d1c021a89f7e9bcdbe8d12c4b779110970e4
pbielenda <p.bielenda@evionica.com>
2022-09-12 10:54:39 +0200

    feat(#2): add filterer balancer ui e2e tests

---
+++ cypress/integration/UI/filtererBalancer.test.js
@@ -0,0 +1,77 @@
+const { FiltererObject } = require('../../POP/filtererObject');
+const { HomePageObject } = require('../../POP/homePageObject');
+const {
+    sendRequestToAddIncomeItem,
+    sendRequestToAddCostItem,
+} = require('../../services/cashFlowManagerService');
+const { TestDataProvider } = require('../../utils/testDataProvider');
+const { faker } = require('@faker-js/faker');
+
+describe('filtererBalancer tests', () => {
+    const generateCashFlowData = (
+        unitCostValue,
+        unitIncomeValue,
+        multiple,
+        specifiedComment
+    ) => {
+        for (let i = 0; i < multiple; i++) {
+            const income = TestDataProvider.customizeReqParamObject(
+                'amount',
+                unitIncomeValue
+            );
+            income.comment = specifiedComment;
+
+            const cost = TestDataProvider.customizeReqParamObject(
+                'amount',
+                unitCostValue
+            );
+            cost.comment = specifiedComment;
+
+            sendRequestToAddIncomeItem(income);
+            sendRequestToAddCostItem(cost);
+        }
+    };
+
+    const generateSpecifiedComment = () =>
+        `${faker.word.noun()}-${faker.random.numeric()}`;
+
+    beforeEach(() => {
+        HomePageObject.visitHomePage();
+        HomePageObject.goToFiltererModule();
+    });
+
+    it('Should return balance = 0 when there is no data', () => {
+        FiltererObject.fillCategory('category');
+        FiltererObject.clickSend();
+
+        FiltererObject.incomeShouldBe(0);
+        FiltererObject.costsShouldBe(0);
+        FiltererObject.balanceShouldBe(0);
+    });
+
+    it('Should return positive balance when incomes are larger than costs', () => {
+        const specifiedComment = generateSpecifiedComment();
+
+        generateCashFlowData(100, 1000, 3, specifiedComment);
+
+        FiltererObject.fillComment(specifiedComment);
+        FiltererObject.clickSend();
+
+        FiltererObject.incomeShouldBe(3000);
+        FiltererObject.costsShouldBe(300);
+        FiltererObject.balanceShouldBe(2700);
+    });
+
+    it('Should return negative balance when costs are larger than incomes', () => {
+        const specifiedComment = generateSpecifiedComment;
+
+        generateCashFlowData(1000, 100, 3, specifiedComment);
+
+        FiltererObject.fillComment(specifiedComment);
+        FiltererObject.clickSend();
+
+        FiltererObject.incomeShouldBe(300);
+        FiltererObject.costsShouldBe(3000);
+        FiltererObject.balanceShouldBe(-2700);
+    });
+});
