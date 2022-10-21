e605d56208d377de1eb54f3e9fcf3403844f7040
Katarzyna Kepa <katarzyna.kepa90@gmail.com>
2022-10-21 12:14:31 +0200

    plugin config fix

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,7 +17,7 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-    config.e2e.excludeSpecPattern = [
+    config.excludeSpecPattern = [
         '**/cypress/integration/1-getting-started/*',
         '**/cypress/integration/2-advanced-examples/*',
         '**/cypress/integration/services/*',
