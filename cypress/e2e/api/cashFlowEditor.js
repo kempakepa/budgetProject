63233969726154befe34643702930e3eefefb41c
pbielenda <p.bielenda@evionica.com>
2022-11-08 07:25:21 +0100

    feat(#14): add very first draft of tests for cash flow editor module

---
+++ cypress/e2e/api/cashFlowEditor.js
@@ -0,0 +1,7 @@
+describe('cashFlowEditor test', () => {
+    it('should return 200 status code if edit cost or income valid', () => {});
+
+    it('should return 404 status code if cost or income not exist', () => {});
+
+    it('should return 400 status code if cost or income not valid', () => {});
+});
---
+++ src/backend/modules/cashFlowEditor/cashFlowEditorIntegration.spec.ts
@@ -0,0 +1,1 @@
+//TODO: add tests to check that filterer and account state returning updated values
